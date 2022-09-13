81935b6aa90c4dc2da6b42279e95fa37df7fd90f
Dani <32429647+danisec@users.noreply.github.com>
2022-09-13 23:06:33 +0700

    update Modal.jsx

--- src/components/UI/organisms/Modal.jsx
+++ src/components/UI/organisms/Modal.jsx
@@ -25,7 +25,6 @@ function Modal({ closeModal }) {
                         </div>
 
                         <div className="flex justify-end items-center p-6 space-x-2 rounded-b border-t border-gray-200">
-
                             <button onClick={() => closeModal(false)} type="button" className="text-gray-900 bg-gray-200 hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10">Close</button>
 
                             <button onClick={() => closeModal(false)} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Save Changes</button>
